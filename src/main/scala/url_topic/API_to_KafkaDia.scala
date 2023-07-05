3442ad92962a2f8016556b5e5840c45ee9a07cc3
Kajal <kajalbhatt93@gmail.com>
2023-07-05 15:52:32 +0100

    kafka to Hive

--- src/main/scala/url_topic/API_to_KafkaDia.scala
+++ src/main/scala/url_topic/API_to_KafkaDia.scala
@@ -32,7 +32,7 @@ object API_to_KafkaDia
 
       messageDF.selectExpr("CAST(ID AS STRING) AS key", "to_json(struct(*)) AS value").selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").write.format("kafka").option("kafka.bootstrap.servers", kafkaServer).option("topic", topicSampleName).save()
 
-      // Thread.sleep(10000) // wait for 10 seconds before making the next call
+      Thread.sleep(10000) // wait for 10 seconds before making the next call
     }
   }
 
