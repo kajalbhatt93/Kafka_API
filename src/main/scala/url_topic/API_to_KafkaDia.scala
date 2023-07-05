0154e488911891b9e10a3853004d9ca87f6a8670
Kajal <kajalbhatt93@gmail.com>
2023-07-05 14:59:12 +0100

    kafka to Hive

--- src/main/scala/url_topic/API_to_KafkaDia.scala
+++ src/main/scala/url_topic/API_to_KafkaDia.scala
@@ -16,7 +16,7 @@ object API_to_KafkaDia
     {
       import spark.implicits._
 
-      val apiUrl = "http://3.9.191.104:7071/api"
+      val apiUrl = "http://127.0.0.1:7071/api"
       val response = get(apiUrl, headers = headers)
       val total = response.text()
       val dfFromText = spark.read.json(Seq(total).toDS)
