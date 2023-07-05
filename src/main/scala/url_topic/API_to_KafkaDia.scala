17bf2d3b9f2ac434cfa433047bcae56b1aa04912
Kajal <kajalbhatt93@gmail.com>
2023-07-05 14:51:48 +0100

    kafka to Hive

--- src/main/scala/url_topic/API_to_KafkaDia.scala
+++ src/main/scala/url_topic/API_to_KafkaDia.scala
@@ -24,7 +24,7 @@ object API_to_KafkaDia
 
       // select the columns you want to include in the message
 
-      val messageDF = dfFromText.select($"Age", $"BMI", $"BloodGlucose_Level", $"Diabetes", $"Gender", $"HbA1c_Level", $"Heart_Disease", $"Hypertension",$"ID", $"Name", $"Smoking_History")
+      val messageDF = dfFromText.select($"ID", $"Name", $"Age", $"Gender", $"Hypertension", $"Heart_Disease",$"Smoking_History", $"BMI",$"HbA1c_Level", $"BloodGlucose_Level", $"Diabetes")
       messageDF.show(10)
       val kafkaServer: String = "ip-172-31-3-80.eu-west-2.compute.internal:9092"
       //val kafkaServer: String = "ip-172-31-3-80.eu-west-2.compute.internal:9092,ip-172-31-5-217.eu-west-2.compute.internal:9092,ip-172-31-13-101.eu-west-2.compute.internal:9092, ip-172-31-9-237.eu-west-2.compute.internal:9092"
@@ -32,10 +32,10 @@ object API_to_KafkaDia
 
       messageDF.selectExpr("CAST(ID AS STRING) AS key", "to_json(struct(*)) AS value").selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").write.format("kafka").option("kafka.bootstrap.servers", kafkaServer).option("topic", topicSampleName).save()
 
-      Thread.sleep(10000) // wait for 10 seconds before making the next call
+      // Thread.sleep(10000) // wait for 10 seconds before making the next call
     }
   }
 
 }
-
+// sudo su hdfs hdfs dfs -rm -r /tmp/jenkins/kafka/heal/checkpoint/*
 // spark-submit --master local[*] --packages "org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.7","com.lihaoyi:requests_2.11:0.7.1" --class url_topic.API_to_KafkaDia target/Kafka_API-1.0-SNAPSHOT.jar
--- src/main/scala/url_topic/Kafka_to_CSVDia.scala
+++ src/main/scala/url_topic/Kafka_to_CSVDia.scala
@@ -3,7 +3,7 @@ package url_topic
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
-object Kafka_to_HiveDia
+object Kafka_to_CSVDia
 {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
@@ -25,21 +25,21 @@ object Kafka_to_HiveDia
     // Define the Kafka topic to subscribe to
     val topic = "test_kj"
 
-    //$"Age", $"BMI", $"BloodGlucose_Level", $"Diabetes", $"Gender", $"HbA1c_Level", $"Heart_Disease", $"Hypertension",$"ID", $"Name", $"Smoking_History"
+    //ID	Name	Gender	Age	Hypertension	Heart_Disease	Smoking_History	BMI	HbA1c_Level	BloodGlucose_Level	Diabetes
 
     // Define the schema for the JSON messages
     val schema = StructType(Seq(
-      StructField("Age", StringType, nullable = true),
-      StructField("BMI", StringType, nullable = true),
-      StructField("BloodGlucose_Level", StringType, nullable = true),
-      StructField("Diabetes", StringType, nullable = true),
-      StructField("Gender", StringType, nullable = true),
-      StructField("HbA1c_Level", StringType, nullable = true),
-      StructField("Heart_Disease", StringType, nullable = true),
-      StructField("Hypertension", StringType, nullable = true),
       StructField("ID", StringType, nullable = true),
       StructField("Name", StringType, nullable = true),
-        StructField("Smoking_History", StringType, nullable = true)
+      StructField("Age", StringType, nullable = true),
+      StructField("Gender", StringType, nullable = true),
+      StructField("Hypertension", StringType, nullable = true),
+      StructField("Heart_Disease", StringType, nullable = true),
+      StructField("Smoking_History", StringType, nullable = true),
+      StructField("BMI", StringType, nullable = true),
+      StructField("HbA1c_Level", StringType, nullable = true),
+      StructField("BloodGlucose_Level", StringType, nullable = true),
+      StructField("Diabetes", StringType, nullable = true)
     ))
 
     // Read the JSON messages from Kafka as a DataFrame
