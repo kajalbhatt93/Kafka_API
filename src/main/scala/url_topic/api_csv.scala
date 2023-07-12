b1de1d1cf9f17a9e5fe1ce1f8ba5dd4679c6b48f
Kajal <kajalbhatt93@gmail.com>
2023-07-12 15:33:32 +0100

    kafka to Hive

--- src/main/scala/url_topic/api_csv.scala
+++ src/main/scala/url_topic/api_csv.scala
@@ -3,24 +3,27 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import requests._
 
+
 object api_csv {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("MyApp")
+      .master("local[*]") // Use all available cores on the local machine
+      .getOrCreate()
+    val topic = "kajal"
+    val brokers = "ip-172-31-3-80.eu-west-2.compute.internal:9092,ip-172-31-5-217.eu-west-2.compute.internal:9092,ip-172-31-13-101.eu-west-2.compute.internal:9092, ip-172-31-9-237.eu-west-2.compute.internal:9092"
+    val url = "http://3.9.191.104:7071/api"
 
-  val spark = SparkSession.builder()
-    .appName("MyApp")
-    .master("local[*]") // Use all available cores on the local machine
-    .getOrCreate()
-  val topic = "kajal"
-  val brokers = "ip-172-31-3-80.eu-west-2.compute.internal:9092,ip-172-31-5-217.eu-west-2.compute.internal:9092,ip-172-31-13-101.eu-west-2.compute.internal:9092, ip-172-31-9-237.eu-west-2.compute.internal:9092"
-  val url = "http://3.9.191.104:7071/api"
-
-  import spark.implicits._
+    import spark.implicits._
 
 
-  val response = get(url)
-  val total = response.text()
-  val dfFromText = spark.read.json(Seq(total).toDS)
-  val kafka_msg = dfFromText.select(to_json(struct($"response"))).toDF("value")
-  kafka_msg.selectExpr("value").write.format("kafka").option("kafka.bootstrap.servers", brokers).option("topic", topic).save()
+    val response = get(url)
+    val total = response.text()
+    val dfFromText = spark.read.json(Seq(total).toDS)
+    val kafka_msg = dfFromText.select(to_json(struct($"response"))).toDF("value")
+    kafka_msg.selectExpr("value").write.format("kafka").option("kafka.bootstrap.servers", brokers).option("topic", topic).save()
+  }
+
 
 
 }
