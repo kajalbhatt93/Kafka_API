86acc83ec15feedcfb642392d5a8de6eb7185606
Kajal <kajalbhatt93@gmail.com>
2023-07-13 16:27:25 +0100

    kafka to Hive

--- src/main/scala/url_topic/Dai_topiccsv.scala
+++ src/main/scala/url_topic/Dai_topiccsv.scala
@@ -44,25 +44,21 @@ object Dai_topiccsv {
     // Read the JSON messages from Kafka as a DataFrame
     val df = spark.readStream.format("kafka")
       .option("kafka.bootstrap.servers", "ip-172-31-3-80.eu-west-2.compute.internal:9092")
-      .option("subscribe", topic).option("startingOffsets", "earliest")
-      .load().select(from_json(col("value").cast("string"), schema)
-      .as("data"))
-      .selectExpr("data.*")
-    df.show(10)
+      .option("subscribe", topic)
+      .option("startingOffsets", "earliest")
+      .load().select(from_json(col("value").cast("string"), schema).as("data")).selectExpr("data.*")
 
     // Write the DataFrame as CSV files to HDFS
     df.writeStream.format("csv")
-      .option("checkpointLocation", "/tmp/jenkins/kafka/emp_data/checkpoint")
-      .option("path", "/tmp/jenkins/kafka/emp_data/data")
+      .option("checkpointLocation", "/tmp/jenkins/kafka/daibetic/checkpoint")
+      .option("path", "/tmp/jenkins/kafka/daibetic/data")
       .start()
       .awaitTermination()
-
-
   }
 }
 
 //sudo -u hdfs hdfs dfs -rm -r /tmp/jenkins/kafka/heal/*
-//sudo -u hdfs hdfs dfs -chmod 777 /tmp/jenkins/kafka/heal/*
+//sudo -u hdfs hdfs dfs -chmod 777 /tmp/jenkins/kafka/daibetic/*
 // hdfs dfs -ls /tmp/jenkins/kafka/heal/
 /*
 use sample;
