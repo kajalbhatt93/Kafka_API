6f551e5c8b31619f65ca0f6bfe47e20feca1f8f9
Kajal <kajalbhatt93@gmail.com>
2023-07-17 11:48:56 +0100

    kafka to Hive

--- src/main/scala/url_topic/Dai_typcast.scala
+++ src/main/scala/url_topic/Dai_typcast.scala
@@ -3,7 +3,8 @@ package url_topic
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
-object Dai_topiccsv {
+
+object Dai_typcast {
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession.builder()
@@ -27,65 +28,38 @@ object Dai_topiccsv {
 
     // Define the schema for the JSON messages
     val schema = StructType(Seq(
-      StructField("Age", StringType, nullable = true),
-      StructField("BMI", StringType, nullable = true),
+      StructField("Age", FloatType, nullable = true),
+      StructField("BMI", FloatType, nullable = true),
       StructField("BloodGlucose_Level", StringType, nullable = true),
-      //StructField("Diabetes", StringType, nullable = true),
       StructField("Gender", StringType, nullable = true),
-      StructField("HbA1c_Level", StringType, nullable = true),
+      StructField("HbA1c_Level", FloatType, nullable = true),
       StructField("Heart_Disease", StringType, nullable = true),
       StructField("Hypertension", StringType, nullable = true),
       StructField("ID", StringType, nullable = true),
       StructField("Name", StringType, nullable = true),
       StructField("Smoking_History", StringType, nullable = true)
-       ))
-    print(schema)
+    ))
 
     // Read the JSON messages from Kafka as a DataFrame
-    val df = spark.readStream.format("kafka")
+    val df = spark.readStream
+      .format("kafka")
       .option("kafka.bootstrap.servers", "ip-172-31-3-80.eu-west-2.compute.internal:9092")
       .option("subscribe", topic)
       .option("startingOffsets", "earliest")
-      .load().select(from_json(col("value").cast("string"), schema).as("data")).selectExpr("data.*")
+      .load()
+      .select(from_json(col("value").cast("string"), schema).as("data"))
+      .selectExpr("CAST(data.Age AS INT) AS AgeInt", "CAST(data.BMI AS INT) AS BMIInt", "data.BloodGlucose_Level", "data.Gender", "CAST(data.HbA1c_Level AS INT) AS HbA1c_LevelInt", "data.Heart_Disease", "data.Hypertension", "data.ID", "data.Name", "data.Smoking_History")
 
     // Write the DataFrame as CSV files to HDFS
-    df.writeStream.format("csv")
-      .option("checkpointLocation", "/tmp/jenkins/kafka/daibetic/checkpoint")
-      .option("path", "/tmp/jenkins/kafka/daibetic/data")
+    df.writeStream
+      .format("csv")
+      .option("checkpointLocation", "/tmp/jenkins/kafka/graph/checkpoint")
+      .option("path", "/tmp/jenkins/kafka/graph/data")
       .start()
       .awaitTermination()
   }
 }
-//62.0,36.39,85,Female,6.1,0,0,60001,Luna Milner,never
-
-//sudo -u hdfs hdfs dfs -rm -r /tmp/jenkins/kafka/daibetic/*
-//sudo -u hdfs hdfs dfs -chmod 777 /tmp/jenkins/kafka/daibetic/*
+// sudo -u hdfs hdfs dfs -chmod 777 /tmp/jenkins/kafka/graph
+//sudo -u hdfs hdfs dfs -rm -r /tmp/jenkins/kafka/graph/*
+//sudo -u hdfs hdfs dfs -chmod 777 /tmp/jenkins/kafka/graph/*
 // hdfs dfs -ls /tmp/jenkins/kafka/heal/
-/*
-use sample;
-CREATE EXTERNAL TABLE sample.realtime_daibetic (
-  Age STRING,
-  BMI STRING,
-  BloodGlucose_Level STRING,
-  Gender STRING,
-  HbA1c_Level STRING,
-  Heart_Disease STRING,
-  Hypertension STRING,
-  ID STRING,
-  Name STRING,
-  Smoking_History STRING
-)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
-STORED AS TEXTFILE
-LOCATION '/tmp/jenkins/kafka/daibetic/data'
-TBLPROPERTIES ("csv.input.fileextension"=".csv");
-
-*/
-/*
-create external table sample.daibetic_hist(Age INT,BMI FLOAT,BloodGlucose_Level INT,Diabetes INT,Gender STRING,HbA1c_Level FLOAT,Heart_Disease INT,Hypertension INT,ID INT,Name STRING,Smoking_History STRING)
-row format delimited
-fields terminated by ','
-stored as textfile
-location '/tmp/kajal/postNifi/';
-*/
