7920c54962301f9eb398e68774f2409d2d4bb8a4
Kajal <kajalbhatt93@gmail.com>
2023-07-19 17:59:03 +0100

    kafka to Hive

---
+++ src/main/scala/url_topic/API_time.scala
@@ -0,0 +1,56 @@
+package url_topic
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import requests._
+import scala.concurrent.duration._
+object API_time
+{
+  def main(args: Array[String]): Unit =
+  {
+    val spark = SparkSession.builder()
+      .appName("My Spark Application")
+      .master("local[*]")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val apiUrl = "http://3.9.191.104:7071/api"
+    val kafkaServer: String = "ip-172-31-3-80.eu-west-2.compute.internal:9092"
+    val topicSampleName: String = "daibetics"
+
+    var lastProcessedID: String = ""
+
+    while (true) {
+      val response = get(apiUrl, headers = headers)
+      val total = response.text()
+      val dfFromText = spark.read.json(Seq(total).toDS)
+
+      val messageDF = dfFromText.select($"Age", $"BMI", $"BloodGlucose_Level", $"Gender", $"HbA1c_Level", $"Heart_Disease", $"Hypertension", $"ID", $"Name", $"Smoking_History")
+        .filter($"ID" > lastProcessedID) // Filter for new data based on ID
+      messageDF.show(10)
+
+      if (!messageDF.isEmpty) {
+        val maxID = messageDF.select(max($"ID")).first().getString(0)
+        lastProcessedID = maxID
+      }
+
+      messageDF
+        .selectExpr("CAST(ID AS STRING) AS key", "to_json(struct(*)) AS value")
+        .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+        .write
+        .format("kafka")
+        .option("kafka.bootstrap.servers", kafkaServer)
+        .option("topic", topicSampleName)
+        .save()
+
+      Thread.sleep(2.minutes.toMillis) // Wait for 2 minutes before making the next API call and pushing to Kafka
+    }
+
+
+  }
+
+
+}
+//spark-submit --master local[*] --packages "org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.7","com.lihaoyi:requests_2.11:0.7.1" --class url_topic.API_time target/Kafka_API-1.0-SNAPSHOT.jar
+//kafka-topics --bootstrap-server ip-172-31-14-3.eu-west-2.compute.internal:9092,ip-172-31-3-80.eu-west-2.compute.internal:9092,ip-172-31-5-217.eu-west-2.compute.internal:9092,ip-172-31-13-101.eu-west-2.compute.internal:9092, ip-172-31-9-237.eu-west-2.compute.internal:9092 --create --topic aaaatest
